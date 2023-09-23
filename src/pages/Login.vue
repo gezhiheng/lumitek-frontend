7c95ce5ff08479490ed955f034779aa2cfd080fd
Henry Ge <1981761687@qq.com>
2023-09-23 17:52:49 +0800

    修改登录页面样式

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,27 +1,23 @@
 <template>
-  <div class="box" style="box-shadow: var(--el-box-shadow-dark);">
-    <img src="../assets/lumitek.jpg" alt="lumitek">
-    <el-input 
-      v-model="staffNoInput" 
-      class="w-50 m-2 staffNoInput" 
-      placeholder="請輸入工號"
-    >
-      <template #prefix>
-        <el-icon class="el-input__icon"><UserFilled /></el-icon>
-      </template>
-    </el-input>
-    <el-input
-      v-model="pwdInput"
-      class="pwdInput"
-      type="password"
-      placeholder="請輸入密碼"
-      show-password
-    >
-      <template #prefix>
-        <el-icon class="el-input__icon"><Lock /></el-icon>
-      </template>
-    </el-input>
-    <el-button class="loginBtn" type="primary">Login</el-button>
+  <div class="main">
+    <div class="box" style="box-shadow: var(--el-box-shadow-dark);">
+      <img src="../assets/lumitek.jpg" alt="lumitek">
+      <el-input v-model="staffNoInput" class="w-50 m-2 staffNoInput" placeholder="請輸入工號">
+        <template #prefix>
+          <el-icon class="el-input__icon">
+            <UserFilled />
+          </el-icon>
+        </template>
+      </el-input>
+      <el-input v-model="pwdInput" class="pwdInput" type="password" placeholder="請輸入密碼" show-password>
+        <template #prefix>
+          <el-icon class="el-input__icon">
+            <Lock />
+          </el-icon>
+        </template>
+      </el-input>
+      <el-button class="loginBtn" type="primary">Login</el-button>
+    </div>
   </div>
 </template>
 
