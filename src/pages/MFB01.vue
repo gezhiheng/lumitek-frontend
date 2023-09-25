ab8959141837578ddf2464591ff8c998392e32ee
Henry Ge <1981761687@qq.com>
2023-09-25 22:45:32 +0800

    初步完成对MFB01的设计

--- src/pages/MFB01.vue
+++ src/pages/MFB01.vue
@@ -1,3 +1,92 @@
 <template>
   <h1>MFB01-晶圓進貨資料</h1>
+
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+        <el-button class="button" el-icon="DocumentAdd">Operation button</el-button>
+        <el-icon><DocumentAdd /></el-icon>
+      </div>
+    </template>
+
+    <MFB01Demo01></MFB01Demo01>
+
+  </el-card>
+
+  <el-card class="box-card-table">
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="總表" name="first">總表</el-tab-pane>
+      <el-tab-pane label="明細" name="second">明細</el-tab-pane>
+      <el-tab-pane label="附件" name="third">附件</el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
+<script setup>
+import { ref } from 'vue'
+import MFB01Demo01 from '../test/components/MFB01-demo01.vue';
+const activeName = ref('first')
+
+const handleClick = (tab, event) => {
+  console.log(tab, event)
+}
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+    disabled: true,
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+</script>
+<style>
+.demo-tabs>.el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+
+h1 {
+  margin-top: 15px;
+}
+
+.box-card {
+  margin: 15px 0 30px 0;
+}
+
+.large-input {
+  margin: 0 20px 0 8px;
+  width: 18em;
+}
+
+.mid-input {
+  margin: 0 20px 0 8px;
+  width: 12em;
+}
+
+.tiny-input {
+  margin: 0px 20px 0 8px;
+  width: 6.5em;
+}
+
+.col {
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/test/components/MFB01-demo01.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="客戶簡碼">
+      <el-select v-model="form.region" placeholder="please select your zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="客戶訂單編號">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    <el-form-item label="單號">
+      <el-input v-model="form.name" placeholder="系統產生" disabled/>
+    </el-form-item>
+    <el-form-item label="進貨日期">
+      <el-input v-model="form.name" disabled/>
+    </el-form-item>
+    <el-form-item label="製程別">
+      <el-input v-model="form.name" disabled/>
+    </el-form-item>
+    <el-form-item label="總晶圓數">
+      <el-input v-model="form.name" disabled/>
+    </el-form-item>
+    <el-form-item label="件別">
+      <el-radio-group v-model="form.resource" disabled>
+        <el-radio label="急件" />
+        <el-radio label="一般" />
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="備註">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
