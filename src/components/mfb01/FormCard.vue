9b610d32f01664f4275ad4b237b3c3e79fe18e07
Henry Ge <1981761687@qq.com>
2023-09-28 14:19:26 +0800

    添加mfb01条件查询，并设置本地化

--- src/components/mfb01/FormCard.vue
+++ src/components/mfb01/FormCard.vue
@@ -24,25 +24,58 @@
         </span>
 
         <el-dialog v-model="dialogFormVisible" title="Shipping address">
-          <el-form :model="form">
-            <el-form-item label="Promotion name" :label-width="formLabelWidth">
-              <el-input v-model="form.name" autocomplete="off" />
+          <el-form :model="form" label-width="120px">
+            <el-form-item label="單號">
+              <el-input v-model="form.name" />
             </el-form-item>
-            <el-form-item label="Zones" :label-width="formLabelWidth">
-              <el-select v-model="form.region" placeholder="Please select a zone">
-                <el-option label="Zone No.1" value="shanghai" />
-                <el-option label="Zone No.2" value="beijing" />
+            <el-form-item label="客戶簡碼">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item label="客戶訂單編號">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item label="Activity zone">
+              <el-select v-model="form.region" placeholder="please select your zone">
+                <el-option label="Zone one" value="shanghai" />
+                <el-option label="Zone two" value="beijing" />
               </el-select>
             </el-form-item>
+            <el-form-item label="Activity time">
+              <el-col :span="11">
+                <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
+              </el-col>
+              <el-col :span="2" class="text-center">
+                <span class="text-gray-500">~</span>
+              </el-col>
+              <el-col :span="11">
+                <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
+              </el-col>
+            </el-form-item>
+            <el-form-item label="Instant delivery">
+              <el-switch v-model="form.delivery" />
+            </el-form-item>
+            <el-form-item label="Activity type">
+              <el-checkbox-group v-model="form.type">
+                <el-checkbox label="Online activities" name="type" />
+                <el-checkbox label="Promotion activities" name="type" />
+                <el-checkbox label="Offline activities" name="type" />
+                <el-checkbox label="Simple brand exposure" name="type" />
+              </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="Resources">
+              <el-radio-group v-model="form.resource">
+                <el-radio label="Sponsor" />
+                <el-radio label="Venue" />
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="Activity form">
+              <el-input v-model="form.desc" type="textarea" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">Create</el-button>
+              <el-button>Cancel</el-button>
+            </el-form-item>
           </el-form>
-          <template #footer>
-            <span class="dialog-footer">
-              <el-button @click="dialogFormVisible = false">Cancel</el-button>
-              <el-button type="primary" @click="dialogFormVisible = false">
-                Confirm
-              </el-button>
-            </span>
-          </template>
         </el-dialog>
 
         <span>
@@ -149,4 +182,5 @@ const options = [
   width: 100%;
   display: flex;
   justify-content: space-between;
-}</style>
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import router from './router/router'
 import App from './App.vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
-.use(ElementPlus)
+.use(ElementPlus, {
+  locale: zhCn,
+})
 .use(router)
 .mount('#app')
