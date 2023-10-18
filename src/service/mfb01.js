bc9e79bfcc95d8e6c17978d96a5630b57e721563
Henry Ge <1981761687@qq.com>
2023-10-18 14:02:04 +0800

    完成资料汇入的逻辑

--- src/service/mfb01.js
+++ src/service/mfb01.js
@@ -1,17 +1,11 @@
 import axios from "axios"
-import swal from 'sweetalert'
 
-const useBackendDataFlag = import.meta.env.VITE_USE_BACKEND_DATA_FLAG === 'true'
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 
 export async function queryMFB01(params) {
-  if (!useBackendDataFlag) {
-    swal('注意', '没有使用后台数据', 'warning')
-    return
-  }
-  try {
-    return await axios.post(`${apiBaseUrl}mfb01/query/`, params)
-  } catch(err) {
-    swal('錯誤', '獲取數據時發生異常', 'error')
-  }
+  return await axios.post(`${apiBaseUrl}mfb01/query/`, params)
+}
+
+export async function importData(params) {
+  return await axios.post(`${apiBaseUrl}mfb01/import-data/`, params)
 }
