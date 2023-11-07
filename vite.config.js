54246765385d8a0b136ae31c667026dae1e48497
Henry Ge <1981761687@qq.com>
2023-11-07 17:18:16 +0800

    整合

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     svgBuilder('./src/assets/icons/')
