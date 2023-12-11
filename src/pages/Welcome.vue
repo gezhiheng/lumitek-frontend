e4bb76d9bbf3c8345b36f4d1c44c2cd52b6e1e86
Henry Ge <1981761687@qq.com>
2023-12-11 17:26:28 +0800

    FEAT
    新增Welcome页面
    夜间模式功能待完成

---
+++ src/pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card class="main">
+    <h1>Welcome</h1>
+  </el-card>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main {
+  margin: 30px 20px 15px 10px;
+  background: #ffffff;
+  width: 97%;
+}
+</style>
