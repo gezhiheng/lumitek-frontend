d1c43139e08228a42d05b08267355793d8bcb0f0
Henry Ge <1981761687@qq.com>
2023-12-22 17:04:25 +0800

    feat: 点击header的logo跳转到welcome页面

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <el-menu mode="horizontal" :ellipsis="false" class="el-menu" @Select="handleSelect">
-    <img v-if="!isDark" src="@/assets/lumitek.jpg" alt="lumitek">
-    <img v-else src="@/assets/lumitek-dark-mode.png" alt="lumitek">
+    <a href="">
+      <img v-if="!isDark" src="@/assets/lumitek.jpg" alt="lumitek">
+      <img v-else src="@/assets/lumitek-dark-mode.png" alt="lumitek">
+    </a>
     <h1 style="line-height: 58px; margin-left: 30px;">{{ title }}</h1>
     <div class="flex-grow" />
     <el-sub-menu :index="feat1.value" v-for="(feat1, feat1Index) in feats1Level" :disabled="feat1.isEmpty">
