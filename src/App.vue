cf3dbc5c7b37e167e93e7cfbc14d319c6c3b5f20
Henry Ge <1981761687@qq.com>
2023-12-26 16:20:02 +0800

    fix: 不同页面显示不同的设置

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,12 @@
 import { ref, watch, provide, onMounted } from 'vue'
 import router from '@/router/router'
 import routerMapping from '@/constants/router_mapping'
+import modeSettings from '@/constants/mode_settings'
+import { useSettingsStore } from '@/stores/settings_store'
 
 const title = ref('')
 const breadcrumbs = ref([])
+const { setModeActive } = useSettingsStore()
 
 watch(() => router.currentRoute.value.path, (newFullPath, oldFullPath) => {
   const name = newFullPath.slice(1)
@@ -28,7 +31,18 @@ watch(() => router.currentRoute.value.path, (newFullPath, oldFullPath) => {
   if (name === 'welcome') {
     breadcrumbs.value = []
   }
+  setSettings(name)
 })
+
+const setSettings = (name) => {
+  modeSettings.forEach(item => {
+    if (item.routerName === name) {
+      Object.keys(item.settings).forEach(key => {
+        setModeActive(key, item.settings[key])
+      })
+    }
+  })
+}
 
 onMounted(() => {
   provide('title', title)
---
+++ src/constants/mode_settings.js
@@ -0,0 +1,18 @@
+const modeSettings = [
+  {
+    routerName: 'mfb01',
+    settings: {
+      darkMode: true,
+      verticalMode: true
+    }
+  },
+  {
+    routerName: 'mfa01',
+    settings: {
+      darkMode: true,
+      verticalMode: false
+    }
+  }
+]
+
+export default modeSettings
