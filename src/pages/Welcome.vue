dda0e1516eee73b190b7b8c145fcdd72cdcf053a
Henry Ge <1981761687@qq.com>
2023-12-31 23:26:16 +0800

    fix: 页面刚开始加载时会有footer上边框

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -31,7 +31,7 @@
         <Suspense><Menu :verticalFlag="false"></Menu></Suspense>
       </template>
     </el-drawer>
-    <el-footer><Footer></Footer></el-footer>
+    <el-footer v-if="state.showDelayedElements"><Footer></Footer></el-footer>
   </div>
 </template>
 
