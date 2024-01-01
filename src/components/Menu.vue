c1b4a269f11a1fd07cf825cf6a0fafccc510d4ec
Henry Ge <1981761687@qq.com>
2024-01-01 14:06:42 +0800

    refactor: menu组件重构（功能列表优化）
    由于菜单加载比较耗时，优化菜单的数据格式，并先使用mock来模拟
    因为优化所以删除路由映射的js文件，并增加了可使用功能的js文件

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -7,94 +7,62 @@
     </router-link>
     <h1>{{ title }}</h1>
     <div class="flex-grow"></div>
-    <el-sub-menu :index="feat1.value" v-for="(feat1, feat1Index) in menu.firstLevel" :disabled="feat1.isEmpty">
-      <template #title>{{ feat1.value }}</template>
-      <div v-for="(feat2, feat2Index) in menu.secondLevel">
-        <div v-if="feat2.index === feat1Index">
-          <el-menu-item v-if="feat2.isEmpty" :index="feat2.value">{{ feat2.value }}</el-menu-item>
-          <el-sub-menu v-else :index="feat2.value">
-            <template #title>{{ feat2.value }}</template>
-            <div v-for="feat3 in menu.thirdLevel">
-              <el-menu-item v-if="feat3.index === feat2Index" :index="feat3.value">
-                {{ feat3.value }}
-              </el-menu-item>
-            </div>
+    <div v-for="feature in featuresRequest.features">
+      <el-sub-menu v-if="feature.children.length > 0" :index="feature.index">
+        <template #title>
+          {{ feature.label }}
+        </template>
+        <div v-for="child1 in feature.children">
+          <el-sub-menu v-if="child1.children && child1.children.length > 0" :index="child1.index">
+            <template #title>
+              {{ child1.label }}
+            </template>
+            <el-menu-item v-for="child2 in child1.children" :index="child2.index">
+              {{ child2.label }}
+            </el-menu-item>
           </el-sub-menu>
+          <el-menu-item v-if="!child1.children" :index="child1.index">
+            {{ child1.label }}
+          </el-menu-item>
         </div>
-      </div>
-    </el-sub-menu>
+      </el-sub-menu>
+    </div>
   </el-menu>
 </template>
 
 <script setup>
-import { reactive, onMounted, inject } from 'vue'
+import { onMounted, inject } from 'vue'
 import { useDark } from "@vueuse/core"
 import { getFeatures }  from '@/service/user'
 import { useFeaturesStore } from '@/stores/features_store'
 import router from '@/router/router'
-import routerMapping from '@/constants/router_mapping'
+import useableFeatures from '@/constants/useable_features'
 
 const isDark = useDark()
-const menu = reactive({
-  firstLevel: [],
-  secondLevel: [],
-  thirdLevel: []
-})
 const title = inject('title')
 const staffNo = window.sessionStorage.getItem('staffNo')
 const useBackendDataFlag = import.meta.env.VITE_USE_BACKEND_DATA_FLAG === 'true'
-const { features, setFeatures } = useFeaturesStore()
+const { featuresRequest, setFeaturesRequest } = useFeaturesStore()
 
 onMounted(async () => {
-  if (useBackendDataFlag && !features.requested) {
+  if (useBackendDataFlag && !featuresRequest.requested) {
     const featuresRequested = await getFeatures(staffNo).then((resolve, reject) => {
       return resolve.data
     })
-    setFeatures(featuresRequested)
+    setFeaturesRequest(featuresRequested)
   }
-  setMenu(features.features)
 })
 
-function setMenu(features) {
-  let index1 = 0
-  let index2 = 0
-  for (const feats1 in features) {
-    const isEmpty = Object.getOwnPropertyNames(features[feats1]).length === 0
-    menu.firstLevel.push({
-      isEmpty: isEmpty,
-      value: feats1
-    })
-    const data2Level = features[feats1]
-    for (const feats2 in data2Level) {
-      const data2 = data2Level[feats2]
-      const isEmpty = Object.getOwnPropertyNames(data2).length === 0
-      menu.secondLevel.push({
-        index: index1,
-        isEmpty: isEmpty,
-        value: feats2
-      })
-      for (const feats3 in data2) {
-        menu.thirdLevel.push({
-          index: index2,
-          value: feats3
-        })
-      }
-      index2++
-    }
-    index1++
-  }
-}
-
 const handleSelect = (index) => {
-  let name = ''
-  routerMapping.forEach(item => {
+  let feature = ''
+  useableFeatures.forEach(item => {
     if (item.index === index) {
-      name = item.name
-      router.push({ name: item.name })
+      feature = item.index
+      router.push({ name: item.index })
       return
     }
   })
-  if (name === '') {
+  if (feature === '') {
     router.push({ name: 'empty' })
   }
 }
---
+++ src/constants/useable_features.js
@@ -0,0 +1,12 @@
+const useableFeatures = [
+  {
+    index: 'mfb01',
+    label: 'MFB01-晶圓進貨資料'
+  },
+  {
+    index: 'mfa01',
+    label: 'MFA01-產品資料維護'
+  }
+]
+
+export default useableFeatures
